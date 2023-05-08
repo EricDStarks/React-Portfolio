12bb6648d4ac27360d041fa421f3a9b9cd580708
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-05-08 12:02:53 -0500

    js

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -27,7 +27,7 @@ class About extends Component {
                 <span style={{ cursor: "auto" }}>
                   <img
                     height="250px"
-                    src={profilepic}
+                    src="images\EricStarks (1).jpg"
                     alt="Avatar placeholder"
                   />
                   <Icon
